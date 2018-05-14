bf8e351a4f7dd855d8c2683db611221d9acffb16
Randy Willey <rwilley966@gmail.com>
2018-05-14 16:04:04 -0400

    Course Learning
    Used for course learning practice

---
+++ Learning_Sample/script.js
@@ -0,0 +1,17 @@
+function Course(title, instructor, level, published, views) {
+    this.title = title;
+    this.instructor = instructor;
+    this.level = level;
+    this.published = published;
+    this.views = views;
+    this.updateViews = function() {
+        return ++this.views;
+    };
+}
+
+var courses = [
+    new Course("JavaScript Essential Training", "Randy Willey", 1, true, 0),
+    new Course("Up and Running", "John Smith", 1, true, 123456)
+];
+
+console.log(courses[1].updateViews());
