9078d906125dc5471f8e05c57da185cbd7491d42
Randy Willey <rwilley966@gmail.com>
2018-06-04 11:22:42 -0400

    Module 04 Project Submission

--- Module_04/project/script.js
+++ Module_04/project/script.js
@@ -1,18 +1,30 @@
 function process() {
     var textIn = document.displayText.textInput.value;
+    var textCount = document.displayText.textInput.value.length + 4;
     var colorIn = document.displayText.textColor.value;
     var textOut = document.createElement("div");
-    textOut.textContent = textIn + "\n";
+
+    for (var i = 0; i < textCount; i++) {
+        textOut.innerHTML += "*";
+    }
+    textOut.innerHTML += "<br />* " + textIn + " *<br />";
+    for (var i = 0; i < textCount; i++) {
+        textOut.innerHTML += "*";
+    }
+
     $(".returnedLabel").append("The text you typed in the color you chose is: ");
     switch (colorIn) {
         case "red":
             $(".returnedResult").css("color", "#a50000");
+            $(".returnedResult").css("font-family", "Courier New");
             break;
         case "green":
             $(".returnedResult").css("color", "#1b9856");
+            $(".returnedResult").css("font-family", "Courier New");
             break;
         case "blue":
             $(".returnedResult").css("color", "#004a87");
+            $(".returnedResult").css("font-family", "Courier New");
             break;
     }
     $(".returnedResult").append(textOut);
